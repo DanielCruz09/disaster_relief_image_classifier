a08d42a1e9e397fc0d41bcb5d7ea1c6d62e40e85
DanielCruz09 <daniel.cruz6121@gmail.com>
2025-11-17 16:35:29 -0600

    Finished data preprocessing script

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,71 +4,76 @@ from PIL import Image
 import os
 from skimage.transform import resize
 import csv
+import zipfile
+import shutil
+from torchvision.transforms import Resize
 
-def generate_labels(images_path:str, category:str) -> pd.DataFrame:
+def organize_files(src:str, dest:str) -> None:
     """
-    Resizes images and generates labels for each image. Images will be resized to 224x224 pixels.
+    Extracts files from a zipfile to a destination directory. 
+    Some files may be redundant, so they will be removed.
+    WARNING: The source directory will be deleted.
 
     Args:
-        images_path (str): Path where the images are stored. 
-        category (str): The type of natural disaster (Fire, Earthquake, Flood, or Normal)
+        src (str): The path to the source directory.
+        dest (str): The path to the destination directory.
 
     Returns:
-        A dataframe containing the image data and labels.
+        None
     """
-    resized_images = []
-    img_dimensions = (224, 224)
-    for filename in os.listdir(images_path):
-        file_path = os.path.join(images_path, filename)
-        for image_file in os.listdir(file_path):
-            full_path = os.path.join(file_path, image_file)
-            if os.path.isfile(full_path):
-                img = Image.open(full_path, mode="r")
-                img_data = np.asarray(img)
-                resized = resize(img_data, img_dimensions)
-                resized_images.append(resized)
+    with zipfile.ZipFile(src, "r") as zip_file:
+        zip_file.extractall(dest)
 
-    data = {
-        "Data": resized_images,
-        "Label": [category for _ in range(len(resized_images))]
-    }
-    df = pd.DataFrame(data=data)
-    return df
+    for filename in os.listdir(dest):
+        path = os.path.join(dest, filename)
+        for filename in os.listdir(path):
+            sub_path = os.path.join(path, filename)
+            shutil.move(sub_path, dest)
 
+        os.rmdir(path)
 
-def write_to_csv(data: pd.DataFrame, split:str, category:str) -> None:
+    os.remove(src)
+
+def resize_images(images_path:str, save_path:str) -> None:
+
     """
-    Writes the data to a CSV file.
+    Resizes images to 224x224 pixels.
 
     Args:
-        data (pandas.DataFrame): The data stored as a Pandas DataFrame.
-        split (str): The type of split (Train, Test, or Val).
-        category (str): The type of natural disaster (Fire, Earthquake, Flood, or Normal)
+        images_path (str): Path where the images are stored.
+        save_path (str): Path where to save the resized images.
 
     Returns:
         None
     """
 
-    path = f"../data/processed/{split}"
-    os.makedirs(path, exist_ok=True)
-    path = f"../data/processed/{split}/{category}.csv"
-    header = "Data,Label"
-    if not os.path.exists(path):
-        with open(path, mode="w", newline="", encoding="utf-8") as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(header)
-
-    data.to_csv(path, index=False)
-
+    img_dimensions = (224, 224)
+    for filename in os.listdir(images_path):
+        file_path = os.path.join(images_path, filename)
+        img = Image.open(file_path, mode="r")
+        resize = Resize(size=img_dimensions)
+        resized_image = resize(img)
+        resized_image.save(os.path.join(save_path, filename))
 
 def main():
     path = "../data/raw/"
-    categories = ["Earthquake", "Fire", "Flood", "Normal"]
     for split in ["Train", "Test", "Val"]:
-        for cat in categories:
-            img_path = os.path.join(path, split)
-            df = generate_labels(img_path, cat)
-            write_to_csv(df, split, cat)
+        src = os.path.join(path, f"{split}.zip")
+        dest = os.path.join(path, split)
+        if not os.path.exists(dest):
+            organize_files(src, dest)
+
+    processed_path = "../data/processed/"
+    for split in ["Train", "Test", "Val"]:
+        for category in ["Earthquake", "Fire", "Flood", "Normal"]:
+            images_path = os.path.join(path, split)
+            images_path = os.path.join(images_path, category)
+
+            save_path = os.path.join(processed_path, split)
+            os.makedirs(save_path, exist_ok=True)
+            save_path = os.path.join(save_path, category)
+            os.makedirs(save_path, exist_ok=True)
+            resize_images(images_path, save_path)
 
 if __name__ == "__main__":
     main()
